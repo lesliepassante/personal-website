0d9f50d1a769f962441fe03d91046266b9f5cff2
Leslie Passante <leslie.passante@gmail.com>
2019-10-04 22:27:48 +0900

    improve placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
 import React from 'react';
 
-export default () => <div>Leslie Passante</div>;
+export default () => (
+  <main>
+    <p
+      style={{
+        fontSize: '20px',
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: 300,
+        margin: '100px 50px'
+      }}
+    >
+      Leslie Passante's website is coming soon. Meanwhile,{' '}
+      <a
+        style={{
+          color: '#909090',
+          textDecoration: 'none'
+        }}
+        href="mailto: leslie.passante@gmail.com"
+      >
+        get in touch
+      </a>
+      .
+    </p>
+  </main>
+);
